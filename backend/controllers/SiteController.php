e1fbcb77390147cfee96855219d6dface2c29065
João Sintra <2220865@my.ipleiria.pt>
2023-11-03 12:37:03 +0000

    Configuração de permissoes

---
+++ backend/controllers/SiteController.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace backend\controllers;
+
+use common\models\LoginForm;
+use Yii;
+use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
+use yii\web\Controller;
+use yii\web\Response;
+
+/**
+ * Site controller
+ */
+class SiteController extends Controller
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::class,
+                'rules' => [
+                    [
+                        'actions' => ['login', 'error','logout'],
+                        'allow' => true,
+                    ],
+                    [
+                        'actions' => ['logout', 'index','teste'],
+                        'allow' => true,
+                        'roles' => ['admin'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::class,
+                'actions' => [
+                    'logout' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => \yii\web\ErrorAction::class,
+            ],
+        ];
+    }
+
+    /**
+     * Displays homepage.
+     *
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+    public function actionTeste(){
+        return $this->render('teste');
+    }
+
+    /**
+     * Login action.
+     *
+     * @return string|Response
+     */
+    public function actionLogin()
+    {
+        if (!Yii::$app->user->isGuest) {
+            return $this->goHome();
+        }
+
+        $this->layout = 'blank';
+
+        $model = new LoginForm();
+        if ($model->load(Yii::$app->request->post()) && $model->login()) {
+            return $this->goBack();
+        }
+
+        $model->password = '';
+
+        return $this->render('login', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Logout action.
+     *
+     * @return Response
+     */
+    public function actionLogout()
+    {
+        Yii::$app->user->logout();
+
+        return $this->goHome();
+    }
+}
---
+++ console/migrations/m231023_114745_init_rbac.php
@@ -0,0 +1,84 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m231023_114745_init_rbac
+ */
+class m231023_114745_init_rbac extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+        $auth = Yii::$app->authManager;
+
+        $permission_fazerEncomendas = $auth->createPermission('fazerEncomendas');
+        $auth->add($permission_fazerEncomendas);
+
+        $permission_alterarDadosPessoais = $auth->createPermission('alterarDadosPessoais');
+        $auth->add($permission_alterarDadosPessoais);
+
+        $permission_gestaoUtilizadores = $auth->createPermission('gestaoUtilizadores');
+        $auth->add($permission_gestaoUtilizadores);
+
+        $permission_gestaoProdutos = $auth->createPermission('gestaoProdutos');
+        $auth->add($permission_gestaoProdutos);
+
+        $permission_gestaoEncomendas = $auth->createPermission('gestaoEncomendas');
+        $auth->add($permission_gestaoEncomendas);
+
+
+        $role_funcionario = $auth->createRole('funcionario');
+        $auth->add($role_funcionario);
+        $auth->addChild($role_funcionario, $permission_gestaoEncomendas);
+        $auth->addChild($role_funcionario, $permission_alterarDadosPessoais);
+
+        $role_gestor = $auth->createRole('gestor');
+        $auth->add($role_gestor);
+        $auth->addChild($role_gestor, $role_funcionario);
+        $auth->addChild($role_gestor, $permission_gestaoProdutos);
+
+        $role_admin = $auth->createRole('admin');
+        $auth->add($role_admin);
+        $auth->addChild($role_admin, $role_gestor);
+        $auth->addChild($role_admin, $permission_gestaoUtilizadores);
+
+        $role_cliente = $auth->createRole('cliente');
+        $auth->add($role_cliente);
+        $auth->addChild($role_cliente, $permission_fazerEncomendas);
+
+        $auth->assign($role_admin, 1);
+        $auth->assign($role_gestor, 2);
+        $auth->assign($role_funcionario, 3);
+        $auth->assign($role_cliente, 4);
+
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $auth = Yii::$app->authManager;
+        $auth->removeAll();
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m231023_114745_init_rbac cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
