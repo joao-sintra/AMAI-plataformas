be86647ca17f53e50ce4a76c772601a953e76e9e
Ruben Amaral <2220848@my.ipleiria.pt>
2023-11-10 12:53:41 +0000

    Template AdminLTE

--- common/config/main.php
+++ common/config/main.php
@@ -2,10 +2,17 @@
 return [
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
-        '@npm'   => '@vendor/npm-asset',
+        '@npm' => '@vendor/npm-asset',
     ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
+        'view' => [
+            'theme' => [
+                'pathMap' => [
+                    '@app/views' => '@vendor/hail812/yii2-adminlte3/src/views'
+                ],
+            ],
+        ],
         'cache' => [
             'class' => \yii\caching\FileCache::class,
         ],
